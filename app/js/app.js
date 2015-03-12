754ee959bd43153af00e37817f4e5009a77556fa
VenCKi <kvkirthy@gmail.com>
2015-03-12 21:39:52 +0530

    Bootstrap integration began.

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,8 @@
 require.config({
 	paths:{
 		"ui-router": "/bower_components/ui-router/release/angular-ui-router.min",
-		"angular": "/bower_components/angular/angular.min"
+		"angular": "/bower_components/angular/angular.min",
+		"bootstrap": "/bower_components/bootstrap/dist/js/bootstrap.min"
 	},
 	shim:{
 		"ui-router":{
@@ -10,12 +11,14 @@ require.config({
 	}
 });
 
-require(['firstController', 'config','ui-router'], function(firstController, config){
+// My main module. This is where everything starts
+require(['firstController', 'config','ui-router', "bootstrap"], function(firstController, config){
 
 	var seedMainModule = angular.module("seedMain", ["ui.router"])
 	.config(config)
 	.controller('firstController', firstController);
 
+	// Manual bootstrap required in the current model with requirejs
 	angular.element(document).ready(function(){
 		angular.bootstrap(document, ['seedMain']);
 		console.log('AngularJS bootstrap complete...');
